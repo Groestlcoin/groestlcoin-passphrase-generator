60f0d9870c364fd9b5b43c01a18aba309ac8b2c8
Gruve_p <gruve-p@users.noreply.github.com>
2018-11-02 14:06:01 +0100

    Port for GRS

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# Bitcoin Random Word Passphrase Generator
-This script can be used to generate random passphrase words for bitcoin wallets.
-
-Script's homepage: [Bitcoin Random Word Passphrase Generator](https://linuxconfig.org/how-to-generate-random-word-passphrase-for-bitcoin-wallet-on-linux-system)
-
+# Groestlcoin Random Word Passphrase Generator
+This script can be used to generate random passphrase words for groestlcoin wallets.
 
 ## Prerequisites
-The **Bitcoin Random Word Passphrase Generator** script requires **/usr/share/dict/words** wordlist file available on your system. Before using this script install wordlist from a standard repository of your Linux distribution.
+The **Groestlcoin Random Word Passphrase Generator** script requires **/usr/share/dict/words** wordlist file available on your system. Before using this script install wordlist from a standard repository of your Linux distribution.
 ```
 UBUNTU / DEBIAN
 # apt install wamerican-small
@@ -15,22 +12,24 @@ CENTOS / REDHAT:
 
 FEDORA:
 # dnf install words
+
+ARCH LINUX:
+# pacman -S words
 ```
 
 ## Usage
-**bitcoin-passphrase-generator.sh** script requires a single argument to determine the number of random words to be generated
+**groestlcoin-passphrase-generator.sh** script requires a single argument to determine the number of random words to be generated
 
 ### Example
 Generate 24 random keywords:
 ```
-$ ./bitcoin-passphrase-generator.sh 24
+$ ./groestlcoin-passphrase-generator.sh 24
 Generating a random word list from 62887 words:
 departures inebriated pecans riddles totter suspender grannie cures preachier refulgent uselessly crumpet swab plainclothesmen agendas calisthenic smothering enclosure pathetic skylarked whined clacks encores slogan
 ```
 Generate 10 random keyword:
 ```
-$ ./bitcoin-passphrase-generator.sh 10
+$ ./groestlcoin-passphrase-generator.sh 10
 Generating a random word list from 62887 words:
 exigencies marchers sublet splicers transcends handling uninformative plowmen lumpiness diuretic
 ```
-
--- groestlcoin-passphrase-generator.sh
+++ groestlcoin-passphrase-generator.sh
@@ -1,14 +1,14 @@
-#!/bin/bash 
- 
-# Bitcoin wallet random passphrase word generator
+#!/bin/bash
+
+# Groestlcoin wallet random passphrase word generator
 # Provided by https://linuxconfig.org
- 
-if [ $# -ne 1 ]; then 
-    echo "Please specify how many random words would you like to generate!" 1>&2 
-    echo "example: ./random-word-generator 5" 1>&2 
-    echo "This will generate 5 random words" 1>&2 
+
+if [ $# -ne 1 ]; then
+    echo "Please specify how many random words would you like to generate!" 1>&2
+    echo "example: ./random-word-generator 5" 1>&2
+    echo "This will generate 5 random words" 1>&2
     exit 1
-fi 
+fi
 
 if [ ! -f "/usr/share/dict/words" ]; then
     echo "Wordlist is not available. To install execute:"
@@ -18,6 +18,8 @@ if [ ! -f "/usr/share/dict/words" ]; then
     echo "yum install words"
     echo "FEDORA:"
     echo "dnf install words"
+    echo "ARCH LINUX:"
+    echo "pacman -S words" 
     exit 1
 fi
 
@@ -30,17 +32,17 @@ ALL_NON_RANDOM_WORDS=`mktemp`
 
 # GENERATE A CLEAN LIST OF NON-RANDOM WORDS BY REMOVING WORDS WITH ACCENT CHARACTERS AND NAMES IDENTIFIED BY CAPITAL LETTERS
 grep "^[a-z]\+$"  /usr/share/dict/words | grep  -vP  "[\x80-\xFF]" > $ALL_NON_RANDOM_WORDS
- 
-# GET A TOTAL NUMBER OF NON-RANDOM WORDS AVAILABLE 
-words_total=`cat $ALL_NON_RANDOM_WORDS | wc -l` 
- 
+
+# GET A TOTAL NUMBER OF NON-RANDOM WORDS AVAILABLE
+words_total=`cat $ALL_NON_RANDOM_WORDS | wc -l`
+
 # WHILE LOOP TO GENERATE REQUESTED NUMBER OF RANDOM KEYWORDS
 echo "Generating a random word list from $words_total words:"
-while [ "$X" -lt "$Y" ]; do 
-    random_number=`od -N3 -An -i /dev/urandom | awk -v f=0 -v r="$words_total" '{printf "%i", f + r * $Y / 16777216}'` 
+while [ "$X" -lt "$Y" ]; do
+    random_number=`od -N3 -An -i /dev/urandom | awk -v f=0 -v r="$words_total" '{printf "%i", f + r * $Y / 16777216}'`
     sed `echo $random_number`"q;d" $ALL_NON_RANDOM_WORDS | tr '\r\n' ' '
-    let "X = X + 1" 
+    let "X = X + 1"
 done
-echo 
+echo
 # REMOVE NON-RANDOM WORD LIST
 rm $ALL_NON_RANDOM_WORDS
